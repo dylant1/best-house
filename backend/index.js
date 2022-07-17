10f7b8a0ad52d9e10f2bb0a2dddbfe1550e4d63b
Dylan Toth <dylant1@yahoo.com>
2022-07-17 11:48:17 -0500

    Initialized frontend

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,5 @@ server.listen(PORT, () => {
 
 io.on("connection", (socket) => {
   /* socket object may be used to send specific messages to the new connected client */
-
   console.log("new client connected");
 });
